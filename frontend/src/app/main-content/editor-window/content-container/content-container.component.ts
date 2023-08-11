d08206749926c095ff0a7b92a0be493671b39112
Andy Jenkins <andrewajenkins@outlook.com>
2023-08-11 00:57:18 -0700

    clean up position stuff (removing, will support input position selection in content menu) fix block elements in content

--- frontend/src/app/main-content/editor-window/content-container/content-container.component.ts
+++ frontend/src/app/main-content/editor-window/content-container/content-container.component.ts
@@ -20,7 +20,6 @@ import {
   ContentAction,
   ContentBridgeService,
 } from '../../../common/services/content-bridge.service';
-import { UiStateManager } from '../../../common/services/ui-state-manager.service';
 
 @Component({
   selector: 'app-content-container',
@@ -32,22 +31,12 @@ export class ContentContainerComponent {
   @ViewChild('scrollMe') private wikiWindow!: ElementRef;
 
   private selectionsSubscription: Subscription;
-  contentSections!: ContentSection[];
   file!: FileTreeFile;
-  position = 0;
-  positionHighlighter: ContentSection = {
-    botID: -1,
-    type: ContentSectionType.HIGHLIGHT,
-    text: 'never show',
-    selected: false,
-  };
-  private positionLocked!: Boolean;
   private fileTreeSubscription: Subscription;
 
   constructor(
     private commandService: CommandService,
     private contentBridgeService: ContentBridgeService,
-    private uiStateManager: UiStateManager,
     private dataService: DataService,
     private cdRef: ChangeDetectorRef
   ) {
@@ -58,8 +47,6 @@ export class ContentContainerComponent {
         if (state.action === ContentAction.ADD_SECTIONS) {
           const content = state.content as ContentSection[];
           this.file?.content.push(...content);
-          // this.contentSections.splice(this.position, 0, ...content);
-          this.position = this.position + content.length;
           this.scrollDown();
         }
       }
@@ -68,18 +55,6 @@ export class ContentContainerComponent {
       if (cmd.action === Action.LOAD_FILE) {
         this.dataService.getFile(cmd.id as number).subscribe((file) => {
           this.file = file;
-          // this.contentSections = file.content;
-          // this.position = file.content.length;
-          // // add highlighter if there is none
-          // const result = this.contentSections.findIndex(
-          //   (content: ContentSection) =>
-          //     content.type === ContentSectionType.HIGHLIGHT
-          // );
-          // if (result >= 0) {
-          //   this.position = result;
-          // } else {
-          //   this.contentSections.push(this.positionHighlighter);
-          // }
           this.scrollDown();
         });
       }
@@ -98,20 +73,6 @@ export class ContentContainerComponent {
     } catch (e) {}
   }
 
-  setPosition(button: ContentSection) {
-    if (button.type === ContentSectionType.HIGHLIGHT || this.positionLocked) {
-      return;
-    }
-    //remove previous
-    this.contentSections.splice(this.position, 1);
-    //set new
-    this.position = this.contentSections.findIndex(
-      (content) => content.uid === button.uid
-    );
-    this.position++; // add highlight button after
-    this.contentSections.splice(this.position, 0, this.positionHighlighter);
-  }
-
   getStyle(type: ContentSectionType) {
     if (type == ContentSectionType.HIGHLIGHT) {
       return {
@@ -124,10 +85,6 @@ export class ContentContainerComponent {
 
   buttonClicked(entry: ContentSection) {
     entry.selected = !entry.selected;
-    if (entry.type === ContentSectionType.HIGHLIGHT) {
-      entry.locked = !entry.locked;
-      this.positionLocked = entry.locked;
-    }
   }
 
   ngOnDestroy() {
