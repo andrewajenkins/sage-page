834686c685ba0033e3ecf7de1a6a4760c9548cbc
Andy Jenkins <andrewajenkins@outlook.com>
2023-08-10 20:58:54 -0700

    fix backend module resolution - refactor pull db stuff to class

--- backend/database/db-manager.ts
+++ backend/database/db-manager.ts
@@ -1,23 +1,66 @@
-const sqlite3 = require("sqlite3").verbose();
-const sqlite = require("sqlite");
+import { Database } from "sqlite";
 
-let db;
+import sqlite3 from "sqlite3";
+import * as sqlite from "sqlite";
 
-export async function getDb() {
-  if (!db) {
-    db = await sqlite.open({
-      filename: "./database/sqlite3.db",
-      driver: sqlite3.Database,
-    });
-    console.log("Opened database successfully", db);
-    db.exec(
-      "create table if not exists treenodes (id integer primary key autoincrement , name text, parent_id int references treenodes(id), type text, parent_type text);"
-    );
-    db.exec("delete from treenodes;");
-  }
-  return db;
-}
-
-class DbManager {
+class DatabaseService {
+  private db: Database | undefined;
   constructor() {}
+
+  public async init() {
+    sqlite
+      .open({
+        filename: "./database/sqlite3.db",
+        driver: sqlite3.Database,
+      })
+      .then((db) => {
+        console.log("Opened database successfully");
+        db.exec(
+          "create table if not exists treenodes (id integer primary key autoincrement , name text, parent_id int references treenodes(id), type text, parent_type text);"
+        );
+        db.exec("delete from treenodes;");
+      });
+  }
+
+  public async getNode(node) {
+    if (!this.db) throw new Error("Database not initialized");
+    await this.db.get("select * from treenodes where id = ?", [node.id]);
+  }
+
+  public async createNode(node: any) {
+    if (!this.db) throw new Error("Database not initialized");
+    await this.db.run(
+      "insert into treenodes (name, parent_id, type, parent_type) values (?,?,?,?)",
+      [node.name, node.parent_id, node.type, node.parent_type]
+    );
+
+    return this.getFileTree();
+  }
+
+  public async getFileTree() {
+    if (!this.db) throw new Error("Database not initialized");
+    return await this.db.all("SELECT * FROM treenodes", []);
+  }
+
+  public async deleteNode(id: number) {
+    if (!this.db) throw new Error("Database not initialized");
+    await this.db.run(
+      `
+WITH RECURSIVE delete_tree AS (
+    SELECT id, parent_id
+    FROM treenodes
+    WHERE id = ?
+    UNION
+    SELECT f.id, f.parent_id
+    FROM treenodes f
+    JOIN delete_tree dt ON f.parent_id = dt.id
+)
+DELETE FROM treenodes
+WHERE id IN (SELECT id FROM delete_tree);
+    `,
+      [id]
+    );
+    return this.getFileTree();
+  }
 }
+export default new DatabaseService();
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,111 +1,41 @@
-const express = require("express");
-const fs = require("fs");
-// import { Database } from "sqlite";
-// const {open as myOpen} = require("sqlite").open;
+import express from "express";
+
 const app = express();
 const port = 3000;
 app.use(express.json());
 
-// db startup
-const sqlite3 = require("sqlite3").verbose();
-const db = require("sqlite")
-  .open({
-    filename: "./database/sqlite3.db",
-    driver: sqlite3.Database,
-  })
-  .then((db) => {
-    console.log("Opened database successfully", db);
-    db.exec(
-      "create table if not exists treenodes (id integer primary key autoincrement , name text, parent_id int references treenodes(id), type text, parent_type text);"
-    );
-    // db.exec("delete from treenodes;");
-    // db.exec("delete from files;");
-    db.close();
-  });
+import db from "../database/db-manager.ts";
 
 app.get("/api/node", async (req: any, res: any) => {
   console.log(req.query);
-
-  const db = await require("sqlite").open({
-    filename: "./database/sqlite3.db",
-    driver: sqlite3.Database,
-  });
-
-  const node = await db.get("select * from treenodes where id = ?", [
-    req.query.id,
-  ]);
-
   const data = {
-    node: node,
+    node: await db.getNode(req.query.id),
   };
   console.log("1 record retrieved: res data:", data);
   res.send(data);
-  db.close();
 });
 
 app.post("/api/node", async (req: any, res: any) => {
   console.log(req.body);
-
-  const db = await require("sqlite").open({
-    filename: "./database/sqlite3.db",
-    driver: sqlite3.Database,
-  });
-
-  await db.run(
-    "insert into treenodes (name, parent_id, type, parent_type) values (?,?,?,?)",
-    [req.body.name, req.body.parent_id, req.body.type, req.body.parent_type]
-  );
-
-  const tree = await db.all("SELECT * FROM treenodes", []);
   const data = {
-    tree: tree,
+    tree: await db.createNode(req.body),
   };
   console.log("1 record inserted: res data:", data);
   res.send(data);
-  db.close();
 });
 app.put("/api/node", (req: any, res: any) => {
-  res.send("Hello World!");
+  res.send("Not yet implemented");
 });
 app.delete("/api/node", async (req: any, res: any) => {
   console.log(req.body, req.query);
-
-  const db = await require("sqlite").open({
-    filename: "./database/sqlite3.db",
-    driver: sqlite3.Database,
-  });
-  const myId = req.body.id;
-  await db.run(
-    // "delete from " + req.body.type + "s where id = " + req.body.id + ";"
-    `
-WITH RECURSIVE delete_tree AS (
-    SELECT id, parent_id
-    FROM treenodes
-    WHERE id = ?
-    UNION
-    SELECT f.id, f.parent_id
-    FROM treenodes f
-    JOIN delete_tree dt ON f.parent_id = dt.id
-)
-DELETE FROM treenodes
-WHERE id IN (SELECT id FROM delete_tree);
-    `,
-    [req.body.id]
-  );
-  const tree = await db.all("SELECT * FROM treenodes", []);
   const data = {
-    tree: tree,
+    tree: await db.deleteNode(req.body.id),
   };
   console.log("1 record inserted: res data:", data);
   res.send(data);
-  db.close();
 });
 app.get("/api/filetree", async (req: any, res: any) => {
-  const db = await require("sqlite").open({
-    filename: "./database/sqlite3.db",
-    driver: sqlite3.Database,
-  });
-  const fileTree = await db.all("SELECT * FROM treenodes", []);
+  const fileTree = await db.getFileTree();
   console.log("query response:", fileTree);
   res.json(fileTree);
 });
