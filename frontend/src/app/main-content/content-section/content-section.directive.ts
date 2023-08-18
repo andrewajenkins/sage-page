71548b269d1ecab0f28264dc337965249e34fe6a
Andy Jenkins <andrewajenkins@outlook.com>
2023-08-18 00:04:50 -0700

    fix bot window clear all not clearing until mouseover

--- frontend/src/app/main-content/content-section/content-section.directive.ts
+++ frontend/src/app/main-content/content-section/content-section.directive.ts
@@ -1,10 +1,4 @@
-import {
-  Directive,
-  HostBinding,
-  HostListener,
-  Input,
-  OnChanges,
-} from '@angular/core';
+import { Directive, HostBinding, HostListener, Input, OnChanges } from '@angular/core';
 import { ComponentLogger } from '../../common/logger/loggers';
 
 @Directive({
@@ -15,35 +9,25 @@ export class ContentSectionDirective implements OnChanges {
   @Input() isSelected = false;
   @HostBinding('style.backgroundColor') backgroundColor;
 
-  private readonly notHoveredColor =
-    'var(--mat-standard-button-toggle-background-color)';
+  private readonly notHoveredColor = 'var(--mat-standard-button-toggle-background-color)';
   private readonly hoveredColor = '#ededed';
-  private readonly selectedColor =
-    'var(--mat-standard-button-toggle-selected-state-background-color)';
+  private readonly selectedColor = 'var(--mat-standard-button-toggle-selected-state-background-color)';
   private readonly notSelectedMouseDownColor = '#d0d0d0';
   private readonly selectedAndHoveredColor = '#f0f0f0';
   ngOnChanges(changes) {
-    this.backgroundColor = this.isSelected
-      ? this.selectedColor
-      : this.hoveredColor;
+    this.backgroundColor = this.isSelected ? this.selectedColor : this.notHoveredColor;
   }
   @HostListener('mouseenter') onMouseEnter() {
-    this.backgroundColor = this.isSelected
-      ? this.selectedAndHoveredColor
-      : this.hoveredColor;
+    this.backgroundColor = this.isSelected ? this.selectedAndHoveredColor : this.hoveredColor;
   }
   @HostListener('mouseleave') onMouseLeave() {
-    this.backgroundColor = this.isSelected
-      ? this.selectedColor
-      : this.notHoveredColor;
+    this.backgroundColor = this.isSelected ? this.selectedColor : this.notHoveredColor;
   }
   @HostListener('mousedown') onMouseDown() {
     this.backgroundColor = this.notSelectedMouseDownColor;
   }
   @HostListener('mouseup') onMouseUp() {
-    this.backgroundColor = this.isSelected
-      ? this.hoveredColor
-      : this.selectedColor;
+    this.backgroundColor = this.isSelected ? this.hoveredColor : this.selectedColor;
   }
   ngOnInit() {
     this.backgroundColor = this.notHoveredColor;
