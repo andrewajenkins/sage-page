0eb51044bd9033c17979748f5377c312d4c56a05
Andy Jenkins <andrewajenkins@outlook.com>
2023-08-20 01:30:34 -0700

    support embedding! listens for 10 messages and has a system object context commands can be added to.

--- frontend/src/app/main-content/bot-window/bot-window.service.ts
+++ frontend/src/app/main-content/bot-window/bot-window.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEvent } from '@angular/common/http';
 import { map, Observable, of } from 'rxjs';
 import { OPENAI_API_KEY, OPENAI_ORG } from '../../../../../tmp/creds';
 import { ServiceLogger } from '../../common/logger/loggers';
 import { tap } from 'rxjs/operators';
-import { botModels, botResponse } from '../../common/data/data';
+import { botResponse } from '../../common/data/data';
+import { Chat } from './chat.model';
 
 interface Model {
   id: string;
@@ -52,6 +53,8 @@ export class BotWindowService {
       'OpenAI-Organization': OPENAI_ORG, // don't need this for text queries
     },
   };
+  log: Chat[] = [];
+  lastQuery = '';
   constructor(private http: HttpClient) {}
   getModels(): Observable<Model[]> {
     return this.http.get<ModelResponse>('https://api.openai.com/v1/models', this.options).pipe(
@@ -63,28 +66,55 @@ export class BotWindowService {
     // }
   }
   postQuery(model = 'gpt-3.5-turbo-16k-0613', query = 'hi bot') {
-    const body = {
-      model: model,
-      messages: [
-        {
-          role: 'user',
-          content: query,
-        },
-      ],
-      temperature: 0.7,
-    };
+    const body = this.prepareRequest(model, query);
     const options = this.options;
     options.headers['Content-Type'] = 'application/json';
-    console.log('making request:', body, options);
     if (this.BYPASS) {
       this.BYPASS = false;
       return of(botResponse);
     } else {
-      return this.http.post<RequestResponse>('https://api.openai.com/v1/chat/completions', body, options).pipe(
-        tap((res) => {
-          // console.log('resp:', JSON.stringify(res));
-        })
-      );
+      this.lastQuery = query;
+      this.log.push(new Chat('user', query));
+      return this.sendQuery(body, options);
     }
+  }
+  sendQuery(body, options) {
+    return this.http
+      .post<RequestResponse>('https://api.openai.com/v1/chat/completions', body, options)
+      .pipe(this.logResults);
+  }
+  prepareRequest(model, query) {
+    return {
+      model: model,
+      messages: this.buildMessages(query),
+      temperature: 0.7,
+    };
+  }
+
+  logResults = tap((response: HttpEvent<RequestResponse>) => {
+    const chat = new Chat('assistant', response);
+    this.log.push(chat);
+  });
+  buildMessages(query: string) {
+    const messages: any = this.getSystemMessages();
+    let logIndex = this.log.length - 1;
+    while (logIndex >= 0) {
+      const chat = this.log[logIndex--];
+      messages.unshift(chat.toObject());
+    }
+    messages.push({
+      role: 'user',
+      content: query,
+    });
+    return messages;
+  }
+
+  private getSystemMessages() {
+    return [
+      {
+        role: 'system',
+        content: 'Please respond in markdown.',
+      },
+    ];
   }
 }
--- frontend/src/app/main-content/bot-window/chat.model.ts
+++ frontend/src/app/main-content/bot-window/chat.model.ts
@@ -1,6 +1,21 @@
 export class Chat {
   id: number;
-  constructor(query, chatResponse) {
+  user!: 'user' | 'assistant' | 'system';
+  content!: string;
+  constructor(user, content) {
     this.id = Math.floor(Math.random() * 1000000);
+    if (user == 'assistant') {
+      this.user = 'assistant';
+      this.content = content.choices[0].message.content;
+    } else {
+      this.user = user;
+      this.content = content;
+    }
+  }
+  toObject() {
+    return {
+      role: this.user,
+      content: this.content,
+    };
   }
 }
