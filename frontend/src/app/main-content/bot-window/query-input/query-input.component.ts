5cdc06ef8e3ab417a486a003c81fbcb6d60e0296
Andy Jenkins <andrewajenkins@outlook.com>
2023-08-28 17:34:25 -0700

    add a path variable to your query from the butotn meny

--- frontend/src/app/main-content/bot-window/query-input/query-input.component.ts
+++ frontend/src/app/main-content/bot-window/query-input/query-input.component.ts
@@ -1,5 +1,9 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ControlValueAccessor, FormGroup } from '@angular/forms';
+import { TreeService } from '../../../common/services/tree.service';
+import { getPath } from '../../../common/utils/tree-utils';
+import { CommandService } from '../../../common/services/command.service';
+import { StateAction } from '../../../common/models/command.model';
 
 @Component({
   selector: 'app-query-input',
@@ -12,13 +16,29 @@ export class QueryInputComponent {
 
   value: string = '';
 
+  constructor(private treeService: TreeService, private commandService: CommandService) {}
+
   submit($event) {
     $event.preventDefault();
     this.query.emit(this.value);
     this.value = '';
   }
 
-  prefillQuery($event: MouseEvent, query) {
-    this.value = query;
+  prefillQuery(action: string, query) {
+    if (action === 'add query') this.value += '\n' + query + '\n';
+    else if (action === 'add context') {
+      if (!this.treeService.currentNode)
+        this.commandService.perform({
+          action: StateAction.NOTIFY,
+          value: 'Please select a file or section to add context to.',
+        });
+      else
+        this.value +=
+          '\nCurrent path in our book/wiki is: ' +
+          getPath(this.treeService.currentNode!, this.treeService.nodeMap)
+            .map((node) => node.name)
+            .join(': ') +
+          '\n';
+    }
   }
 }
